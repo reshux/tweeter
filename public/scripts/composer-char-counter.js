f647570d012981ee1b5056679d31e72a03ead968
reshux <duruhanunsal@gmail.com>
2019-03-20 20:42:53 +0000

    Redirect after changing appropriate input from class to ID

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,14 +2,11 @@ $(document).ready(function() {
   $("#tweet-area")
     .keyup(function() {
       let value = $(this).val().length;
-      $("span.counter").text(140 - value);
+      $("#counter").text(140 - value);
       if (140 - value < 0) {
-        $("span.counter").css("color", "red");
+        $("#counter").css("color", "red");
       } else {
-        $("span.counter").css("color", "black");
-      }
-      if (value === 0) {
-        $("span.counter").text(140);
+        $("#counter").css("color", "black");
       }
     })
     .keyup();
